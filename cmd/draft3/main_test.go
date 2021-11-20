7c186c44ac9e3d7008e5a76480df884b616f6ddb
Loh Siu Yin <siuyin@beyondbroadcast.com>
2021-11-20 06:16:19 +0000

    Fixed bad comments in draft3 test.

--- cmd/draft3/main_test.go
+++ cmd/draft3/main_test.go
@@ -7,10 +7,10 @@ func TestActuate(t *testing.T) {
 		i int
 		o int
 	}{
-		{i: 24, o: -1}, // case:0, input 24, expect output to be -1 (cooling)
-		{i: 25, o: 0},
-		{i: 30, o: 0},
-		{i: 31, o: 1},
+		{i: 24, o: -1}, // case: 0, input 24, expect output to be -1 (too cold)
+		{i: 25, o: 0},  // case: 1, input 25, expected output 0 (just right)
+		{i: 30, o: 0},  // case: 2, input 30, exppected output 0 (just right)
+		{i: 31, o: 1},  // case: 3, input 31, expected output 1 (too hot)
 	}
 
 	for n, d := range dat {
